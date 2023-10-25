a89e8353c6118f24aecbc2af2395db9bea41fe30
Erick de Brito Melo <erick.melo@sorocaba.sp.gov.br>
2023-10-25 07:37:24 -0300

    Card component

--- App.js
+++ App.js
@@ -2,6 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import Header from './components/Header';
+import CardTodo from './components/CardTodo';
+
+const TODO_LIST = [
+  { id: 1, title: "Walk the dog", isCompleted: true },
+  { id: 2, title: "Go to the dentist", isCompleted: false },
+  { id: 3, title: "Learn React Native", isCompleted: false },
+]
 
 export default function App() {
   return (
@@ -12,7 +19,7 @@ export default function App() {
             <Header />
           </View>
           <View style={s.body}>
-            <Text>Body</Text>
+            <CardTodo todo={TODO_LIST[0]}/>
           </View>
         </SafeAreaView>
       </SafeAreaProvider>
---
+++ components/CardTodo.jsx
@@ -0,0 +1,41 @@
+import { Text, Image, StyleSheet } from "react-native"
+import { TouchableOpacity } from "react-native"
+import checkImg from '../assets/check.png'
+
+export default function CardTodo(props) {
+    return (
+        <TouchableOpacity style={s.card}>
+            <Text style={[props.todo.title, props.todo.isCompleted && {textDecorationLine: "line-through"}]}>
+                {props.todo.title}
+            </Text>
+            { props.todo.isCompleted && <Image style={s.img} source={checkImg}/> }
+        </TouchableOpacity>
+    )
+}
+
+const s = StyleSheet.create({
+    card: {
+        backgroundColor: "white",
+        height: 115,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+        borderRadius: 13,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        paddingHorizontal: 20,
+    },
+    title: {
+        fontSize: 25,
+    },
+    img: {
+        height: 25,
+        width: 25
+    }
+})
