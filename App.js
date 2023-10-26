15e050ef65ebfb527b741d596a821b046dfaec65
Erick de Brito Melo <erick.melo@sorocaba.sp.gov.br>
2023-10-26 13:02:00 -0300

    Scrollable list of cards

--- App.js
+++ App.js
@@ -1,16 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import Header from './components/Header';
 import CardTodo from './components/CardTodo';
+import { useState } from 'react';
 
-const TODO_LIST = [
-  { id: 1, title: "Walk the dog", isCompleted: true },
-  { id: 2, title: "Go to the dentist", isCompleted: false },
-  { id: 3, title: "Learn React Native", isCompleted: false },
-]
 
 export default function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: "Walk the dog", isCompleted: true },
+    { id: 2, title: "Go to the dentist", isCompleted: false },
+    { id: 3, title: "Learn React Native", isCompleted: false },
+    { id: 4, title: "Walk the dog", isCompleted: true },
+    { id: 5, title: "Go to the dentist", isCompleted: false },
+    { id: 6, title: "Learn React Native", isCompleted: false },
+    { id: 7, title: "Walk the dog", isCompleted: true },
+    { id: 8, title: "Go to the dentist", isCompleted: false },
+    { id: 9, title: "Learn React Native", isCompleted: false },
+  ])
+
+  function renderTodoList(){
+    return todoList.map((todo) => (
+      <View key={todo.id} style={s.cardItem}>
+        <CardTodo todo={todo} />
+      </View>
+      )
+    )
+  }
   return (
     <>
       <SafeAreaProvider>
@@ -19,7 +35,9 @@ export default function App() {
             <Header />
           </View>
           <View style={s.body}>
-            <CardTodo todo={TODO_LIST[0]}/>
+            <ScrollView>
+              {renderTodoList()}
+            </ScrollView>
           </View>
         </SafeAreaView>
       </SafeAreaProvider>
@@ -36,6 +54,9 @@ const s = StyleSheet.create({
     backgroundColor: "#F9F9F9",
     padding: 15
   },
+  cardItem: {
+   marginBottom: 15 
+  },
   header: {
     flex: 1,
   },
@@ -44,5 +65,6 @@ const s = StyleSheet.create({
   },
   footer: {
     height: 70,
+    backgroundColor: "white"
   }
 });
